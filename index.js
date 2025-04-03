8ce4a7369df48705edd1003850c9ad98e4370f6c
KaushalSojitra1911 <sujalpaghadal17@gmail.com>
2025-04-03 11:14:42 +0530

    changes

--- index.js
+++ index.js
@@ -17,10 +17,7 @@ const PORT = process.env.PORT || 8080;
 connectionDB();
 
 // CORS Middleware
-app.use(cors({
-    origin: "https://weddingcard-steel.vercel.app",
-    credentials: true
-}));
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}));
 
 // Middleware
 app.use(express.json());
